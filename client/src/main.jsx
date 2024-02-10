de81b60bc53bc3e574f539ecd6d9823c3c35b0c8
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-02-10 11:26:18 -0600

    Import themes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "./css/dark.css"
+import "./css/light.css"
 import { BrowserRouter as Router} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
