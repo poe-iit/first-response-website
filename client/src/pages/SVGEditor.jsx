96de6d75386d29f35a3f85243f7708c03a6d3ce0
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-02-10 11:27:22 -0600

    Create svg editor

---
+++ client/src/pages/SVGEditor.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const SVGEditor = () => {
+  return (
+    <Container>
+      <svg>
+        <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/>
+      </svg>
+    </Container>
+  )
+}
+
+export default SVGEditor
+
+const Container = styled.div``
