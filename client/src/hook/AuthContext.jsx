8fa471ff49c05163cb92be63e66cd87a74a52d0c
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-02-12 11:54:39 -0600

    Add global data field

--- client/src/hook/AuthContext.jsx
+++ client/src/hook/AuthContext.jsx
@@ -3,8 +3,11 @@ import { createContext } from "react";
 export const AuthContext = createContext({
   app: null,
   auth: null,
+  db: null,
   user: null,
   setUser: () => {},
+  userData: {},
+  setUserData: () => {},
   loading: false,
   setLoading: () => {}
 })
