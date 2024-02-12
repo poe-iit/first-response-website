e65a0e15e3b0b31c641f90d93a22bf8cd3f74071
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-02-12 11:53:54 -0600

    Create component for displaying error

---
+++ client/src/components/ErrorBlob.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+import ErrorOutline  from '@mui/icons-material/ErrorOutline'
+
+const ErrorBlob = ({title, message}) => {
+  return (
+    <Container>
+      <ErrorOutline sx={{
+        color: "var(--md-sys-color-on-error-container)",
+        width: "1.35em",
+        height: "1.35em"
+      }}/>
+      <h1>{title}</h1>
+      <p>{message}</p>
+    </Container>
+  )
+}
+
+export default ErrorBlob
+
+const Container = styled.div`
+  background-color: var(--md-sys-color-error-container);
+  width: 100%;
+  padding: 0.5em 1em 1em 1em;
+  font-size: 0.95em;
+  border-radius: 0.7em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.2em;
+  h1{
+    font-size: 1.4em;
+    color: var(--md-sys-color-on-error-container);
+  }
+  p{
+    color: var(--md-sys-color-on-error-container);
+    font-size: 1em;
+    font-weight: 400;
+    text-align: center;
+  }
+`
