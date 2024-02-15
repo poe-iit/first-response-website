d1bfb016816d97f0bd537d5ac50c5fb5850e9c6f
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-02-15 11:01:18 -0600

    Add realtime updates

--- client/src/components/FloorDropDown.jsx
+++ client/src/components/FloorDropDown.jsx
@@ -1,37 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { initializeApp } from 'firebase/app'
-import { getFirestore, getDocs, collection } from 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_CONFIG_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_CONFIG_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_CONFIG_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_CONFIG_STORAGE_BUCKET,
-  messagingSenderId:import.meta.env.VITE_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_CONFIG_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_CONFIG_MEASUREMENT_ID
-};
+import React, { useContext, useEffect, useState } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { AuthContext } from '../hook/AuthContext'
 
 
 const FloorDropDown = ({ floorId, setFloorId}) => {
 
   // Create a usecontext to reduce api calls to firestore
-  const app = initializeApp(firebaseConfig)
-  const db = getFirestore(app)
+  const { db } = useContext(AuthContext)
   const [floors, setFloors] = useState([])
 
-  const getFloors = async () => {
-    const collectionRef = collection(db, "floors")
-    const collectionSnap = await getDocs(collectionRef)
-    setFloors(collectionSnap.docs.map(doc => {return {
-      ...doc.data(),
-      id: doc.id
-    }}))
-  }
-
   useEffect(() => {
-    getFloors()
-  }, [])
+    if(db){
+      (async () => {
+        const collectionRef = collection(db, "buildings", "DF6QbHKTyxHlBnf4MBeZ", "floors")
+        const collectionSnap = await getDocs(collectionRef)
+        setFloors(collectionSnap.docs.map(doc => {return {
+          ...doc.data(),
+          id: doc.id
+        }}))
+      })()
+    }
+  }, [db])
   return (
     <select defaultValue={floorId} onChange={e => setFloorId(e.target.value)}>
       <option value={""}>Select a floor</option>
