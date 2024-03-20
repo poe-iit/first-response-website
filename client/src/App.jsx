ab1d81660e9a1db81faa75746313666abe3a7924
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-03-20 16:59:33 -0500

    Create generic comment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,17 @@ import SVGEditor from "./pages/SVGEditor"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import { AuthContext } from "./hook/AuthContext"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { initializeApp } from "firebase/app"
 import { getAuth, getRedirectResult, onAuthStateChanged } from "firebase/auth";
 import { get, ref } from "firebase/database"
 import { getDatabase } from "firebase/database"
 import Home from "./pages/Home"
 import AccountConfirmation from "./pages/AccountConfirmation"
+import NewAdmin from "./pages/NewAdmin"
+import { WebSocketPage } from "./pages/WebSocket"
+import EditFloor from "./pages/EditFloor"
+import ErrorPage from "./pages/ErrorPage"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_CONFIG_API_KEY,
@@ -33,8 +37,50 @@ function App() {
   const [user, setUser] = useState(null)
   const [userData, setUserData] = useState({})
   const [loading, setLoading] = useState(false)
+  const [description, setDescription] = useState("")
+  const descriptionRef = useRef()
+  const element = useRef()
 
   const navigate = useNavigate() 
+
+
+  useEffect(() => {
+    const handleMouseOver = (e) => {
+      e.stopPropagation()
+      if(e.target.dataset.title){
+        element.current = e.target
+        setDescription(e.target.dataset.title)
+      }
+      else if(!e.target.parentNode?.dataset?.title){
+        setDescription("")
+      }
+    }
+    window.addEventListener("mouseover", handleMouseOver)
+    return () => {
+      window.removeEventListener("mouseover", handleMouseOver)
+    }
+  }, [])
+
+  useEffect(() => {
+    if(element.current && description.length){
+      const elementRect = element.current.getBoundingClientRect()
+      const descriptionRect = descriptionRef.current.getBoundingClientRect()
+      const gap = 10
+      let y = elementRect.top - gap - descriptionRect.height
+      let x = elementRect.left + elementRect.width / 2 - descriptionRect.width / 2
+      if(y < gap){
+        y = elementRect.bottom + gap
+      }
+      // console.log("working")
+      // console.log(e)
+      // let x = e.clientX - e.offsetX - descriptionRef.current.clientWidth / 2 + e.target.clientWidth / 2
+      // const y = e.clientY - e.offsetY - descriptionRef.current.clientHeight / 2 + e.target.clientHeight / 2
+      descriptionRef.current.style.top = y + "px"
+      descriptionRef.current.style.left =  x + "px"
+    }else{
+      descriptionRef.current.style.top = "-100px"
+    }
+  }, [description])
 
   useEffect(() => {
     setApp(initializeApp(firebaseConfig))
@@ -96,7 +142,12 @@ function App() {
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<SignUp />} />
           <Route exact path="/account-confirmation" element={<AccountConfirmation />} />
+          <Route exact path="/new-admin" element={<NewAdmin />} />
+          <Route exact path="/websocket" element={<WebSocketPage />} />
+          <Route exact path="/floor/:floor" element={<EditFloor />} />
+          <Route exact path="/404" element={<ErrorPage />} />
         </Routes>
+        <div ref={descriptionRef} id="meta-description" className = {description.length ? "active" : ""}>{description}</div>
       </Container>
     </AuthContext.Provider>
   )
@@ -106,6 +157,19 @@ export default App
 
 const Container = styled.div`
   background-color: var(--md-sys-color-background);
+  #meta-description{
+    position: fixed;
+    font-size: 0.9em;
+    top: 100px;
+    padding: 0.6em;
+    background-color: var(--md-sys-color-primary);
+    color: var(--md-sys-color-on-primary);
+    border-radius: 10px;
+    z-index: 100;
+    &.active{
+      display: block;
+    }
+  }
   > svg{
     position: absolute;
     top: 0;
--- client/src/components/MainControls.jsx
+++ client/src/components/MainControls.jsx
@@ -6,25 +6,39 @@ import LockOpenIcon from '@mui/icons-material/LockOpen';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
 import CenterFocusWeakIcon from '@mui/icons-material/CenterFocusWeak';
-import { set, ref } from "firebase/database"
-import { AuthContext } from '../hook/AuthContext';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import PolylineIcon from '@mui/icons-material/Polyline';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import RoomPreferencesIcon from '@mui/icons-material/RoomPreferences';
+import PublishIcon from '@mui/icons-material/Publish';
 import styled from 'styled-components';
+import { CanvasContext } from '../hook/CanvasContext';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import { Link } from 'react-router-dom';
 
-const MainControls = ({locked, setLocked, setNodes, nodes, setSvgPosition, setSvgScale, positionRef, floorId, setMouseState, mouseState}) => {
+const MainControls = ({positionRef, setUploadPlan}) => {
 
-  const { db } = useContext(AuthContext)
+  const {nodes, mouseState, setMouseState, setSvgPosition, setSvgScale, locked, setLocked, floor, state } = useContext(CanvasContext)
 
   useEffect(() => {
     localStorage["locked"] = JSON.stringify(locked)
   }, [locked])
 
   const resetNodes = () => {
+    if(!floor?.id)return
     const tempNodes = JSON.parse(JSON.stringify(nodes))
     for(const nodeId in tempNodes){
       tempNodes[nodeId].state = "safe"
-      set(ref(db, `buildings/DF6QbHKTyxHlBnf4MBeZ/floors/${floorId}/nodes/${nodeId}/state`), "safe")
+      fetch(`${import.meta.env.VITE_SERVER_URL}/floor/${floor.id}/nodes`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          [nodeId]: tempNodes[nodeId].state
+        })
+      })
     }
-    setNodes(tempNodes)
   }
 
 
@@ -32,48 +46,78 @@ const MainControls = ({locked, setLocked, setNodes, nodes, setSvgPosition, setSv
     <Container>
       <button onClick={() => {
         setLocked(!locked)
-      }}>
+      }} data-title={ !locked ? "Lock Canvas " : "Unlock Canvas "}>
         {
           locked ? <LockIcon sx={{
             fontSize: "1.3em",
-          }}/> : <LockOpenIcon sx={{
+          }} data-title="Unlock Canvs "/> : <LockOpenIcon sx={{
             fontSize: "1.3em",
-          }}/>
+          }} data-title="Lock Canvas"/>
         }
       </button>
-      <button onClick={() => setMouseState("default")} className={mouseState === "default" ? "active" : ""}>
-        <Cursor />
+      <button onClick={() => setMouseState("default")} className={mouseState === "default" ? "active" : ""} data-title="Move Items">
+        <Cursor data-title="Move Items"/>
       </button>
-      <button onClick={() => setMouseState("danger")} className={mouseState === "danger" ? "active" : ""}>
+      {state === "view" && <button onClick={() => setMouseState("danger")} className={mouseState === "danger" ? "active" : ""} data-title="Set Node On Fire">
         <WhatshotIcon sx={{
         fontSize: "1.3em",
-      }}/>
-      </button>
-      <button onClick={resetNodes}>
+      }} data-title="Set Node On Fire"/>
+      </button>}
+     {state !== "view" && <button onClick={() => setMouseState("circle")} className={mouseState === "circle" ? "active" : ""} data-title="Create Node">
+        <AddCircleOutlineIcon sx={{
+        fontSize: "1.3em",
+      }} data-title="Create Node"/>
+      </button>}
+      {state !== "view" && <button onClick={() => setMouseState("line")} className={mouseState === "line" ? "active" : ""} data-title="Link Nodes">
+        <PolylineIcon sx={{
+        fontSize: "1.3em",
+      }} data-title="Link Nodes"/>
+      </button>}
+      {state !== "view" && <button onClick={() => setMouseState("delete")} className={mouseState === "delete" ? "active" : ""} data-title="Delete Node/Line">
+        <DeleteOutlineIcon sx={{
+        fontSize: "1.3em",
+      }} data-title="Delete Node/Line"/>
+      </button>}
+      {state !== "view" && <button onClick={() => setMouseState("exit")} className={mouseState === "exit" ? "active" : ""} data-title="Toggle Exit Node">
+        <RoomPreferencesIcon sx={{
+        fontSize: "1.3em",
+        }} data-title="Toggle Exit Node"/>
+      </button>}
+      {state === "view" && <button onClick={resetNodes} data-title="Reset Nodes">
         {/* Reset all nodes */}
         <RestartAltIcon sx={{
         fontSize: "1.3em",
-      }} />
-      </button>
+      }} data-title="Reset Nodes"/>
+      </button>}
       <button onClick={() => {
         setSvgPosition([0, 0])
         positionRef.current = [0, 0]
-      }}> 
+      }} data-title="Center Plan"> 
       {/* Center to screen with this one */}
         <CenterFocusWeakIcon sx={{
         fontSize: "1.3em",
-      }}/>
+      }} data-title="Center Plan"/>
       </button>
       <button onClick={() => {
         setSvgPosition([0, 0]);
         setSvgScale(1)
         positionRef.current = [0, 0]
-      }}>
+      }} data-title="Reset Canvas">
         {/* Revert to original scale with this one */}
         <CenterFocusStrongIcon sx={{
         fontSize: "1.3em",
-      }}/>
+      }} data-title="Reset Canvas"/>
       </button>
+      {state !== "view" && <button onClick={() => setUploadPlan(true)} data-title="Upload Plan">
+        <PublishIcon sx={{
+        fontSize: "1.3em",
+        }} data-title="Upload Plan"/>
+      </button>}
+      {state === "view" && <Link to={`/floor/${floor?.id}`} data-title="Edit Plan">
+        <OpenInNewIcon sx={{
+        fontSize: "1.3em",
+        }} data-title="Edit Plan"/>
+      </Link>}
     </Container>
   )
 }
@@ -92,7 +136,8 @@ const Container = styled.div`
   border-radius: 5px;
   padding: 0.2em;
   gap: 0.2em;
-  button{
+  button, a{
+    font-size: 0.9em;
     outline: 0;
     border: 0;
     background-color: transparent;
