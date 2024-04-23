e9688b9db2a88bb0ed8785affb807eff5b84199b
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-04-23 18:17:23 -0500

    Update state logic

--- client/src/components/AdminNodes.jsx
+++ client/src/components/AdminNodes.jsx
@@ -2,13 +2,13 @@ import AdminNode from './AdminNode'
 import { useContext } from 'react'
 import { CanvasContext } from '../hook/CanvasContext'
 
-const AdminNodes = ({ setNodeSelected }) => {
+const AdminNodes = ({ setNodeSelected, setEditingNode, setEditNode }) => {
   const { nodes } = useContext(CanvasContext)
   return (
     <>
       {
         nodes && Object.keys(nodes).map((key) => {
-          return <AdminNode key={key} id={key}setNodeSelected={setNodeSelected} />
+          return <AdminNode key={key} id={key} setNodeSelected={setNodeSelected} setEditingNode={setEditingNode} setEditNode={setEditNode} />
         })
       }
     </>
