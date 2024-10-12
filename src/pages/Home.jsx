e68acf1e23fdcc7ecc4a68e2fab47dae4efd11e1
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 18:16:14 -0500

    Add logout button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { AuthContext } from "../hooks/AuthContext"
 
 const Home = () => {
+  const { setUser } = useContext(AuthContext)
+  const logout = () => {
+    const query = `
+      query {
+        logoutUser {
+          message
+          status
+        }
+      }
+    `
+
+    fetch(`${import.meta.env.VITE_SERVER_URI}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({ query })
+    }).then(
+      res => res.json()
+    ).then(
+      res => {
+        setUser(null)
+        console.log(res)
+      }
+    ) 
+  }
   return (
     <Container>
       <h1>Home</h1>
@@ -13,6 +42,11 @@ const Home = () => {
           <p>you can delete the cookie to see the login page again</p>
         </li>
       </ol>
+
+      <p>Here's the logout button</p>
+      <button onClick={(e) => {
+        logout()
+      }}>Logout</button>
     </Container>
   )
 }
