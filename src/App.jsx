6a4548170a51387cfb19cf1fce5d8d071b28f2dc
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 04:03:25 -0500

    Implement authentication logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import Login from './pages/Login'
+import { AuthContext } from './hooks/AuthContext'
+import { useState, useEffect } from 'react'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [user, setUser] = useState(null)
+  const [isAuth, setIsAuth] = useState(false)
+
+  useEffect(() => {
+    if (!user) {
+      const query = `
+        query {
+          validateSession {
+            id
+            username
+            email
+            accountStatus
+            roles
+          }
+        }
+      `
+
+      fetch(`${import.meta.env.VITE_SERVER_URI}/graphql`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({ query })
+      }).then(
+        res => res.json()
+      ).then(
+        res => {
+          console.log(res)
+          if(res?.data?.validateSession){
+            setUser(res.data.validateSession)
+          }
+        }
+      )
+    }
+  }, [])
+
+  useEffect(() => {
+    if(user) {
+      setIsAuth(true)
+    }
+  }, [user])
 
   return (
     <AuthContext.Provider value={{ user, setUser, isAuth, setIsAuth }}>
@@ -22,3 +66,4 @@ const Container = styled.div`
 `
 
 export default App
+
