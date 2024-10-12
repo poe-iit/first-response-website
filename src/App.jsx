2f02a91ca16d4d9b4d35764ef52c79fbea6562a6
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 04:03:00 -0500

    Define Routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthContext.Provider value={{ user, setUser, isAuth, setIsAuth }}>
+      <Container>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+        </Routes>
+      </Container>
+    </AuthContext.Provider>
   )
 }
+
+const Container = styled.div` 
+`
 
 export default App
