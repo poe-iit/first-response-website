3b13eac594293a0de8ad7978285f89d5ec018ce4
Tayomide <tibrahim@hawk.iit.edu>
2024-10-16 23:12:36 -0500

    Update home with prototype link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,13 +35,19 @@ const Home = () => {
     <Container>
       <h1>Home</h1>
       <p>There isn't anything here currently so I'll use it as a sort of list of created pages</p>
-      <ol>
+      <ul>
         <li>
           <Link to="/login">Login</Link>
           <p>Once you're logged in, the page will redirect you to the dashboard</p>
           <p>you can delete the cookie to see the login page again</p>
+          <br />
         </li>
-      </ol>
+        <li>
+          <Link to="/prototype1">Prototype 1</Link>
+          <p>This page is to test out the new node creation api</p>
+          <br />
+        </li>
+      </ul>
 
       <p>Here's the logout button</p>
       <button onClick={(e) => {
