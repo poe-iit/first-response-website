e40d5b1b979f066cd1ce992c3f4c9276bd1ed900
andrewzhang451 <zhangandrew2020@gmail.com>
2024-10-17 21:53:25 -0500

    initial commit

---
+++ src/components/script.js
@@ -0,0 +1,8 @@
+// Remember  to link this file and css back to main webpage later on
+
+document.addEventListener("DOMContentLoaded", function() {
+    const usernameElement = document.querySelector('.username');
+    const username = "John Doe";  // You can replace this with a dynamic variable later on as well; this will be place holder
+    usernameElement.textContent = username;
+});
+
