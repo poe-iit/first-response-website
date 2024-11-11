c16c879996c28387371b9b9522e6d92aa1ab72fb
Tayomide <tibrahim@hawk.iit.edu>
2024-11-11 14:34:13 -0600

    Temporarily remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <Router>
       <App />
     </Router>
-  </StrictMode>,
+  // </StrictMode>,
 )
