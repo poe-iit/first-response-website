9dd68d043f479d5a638007277c9859eda8cebb17
Tayomide <tibrahim@hawk.iit.edu>
2024-11-11 14:35:17 -0600

    Update line connection logic

--- src/components/ConnectedLines.jsx
+++ src/components/ConnectedLines.jsx
@@ -2,20 +2,17 @@ import { useContext } from 'react'
 import { Line } from 'react-konva'
 import { CanvasContext } from '../hooks/CanvasContext'
 
-const ConnectedLines = ({ connectionData }) => {
+const ConnectedLines = ({ firstNodeName, secondNodeName }) => {
   const { nodes } = useContext(CanvasContext)
-  const { connectedNodes } = connectionData
-  const firstNode = nodes.get(connectedNodes[0].name)
-  const secondNode = nodes.get(connectedNodes[1].name)
+  const firstNode = nodes.get(firstNodeName)
+  const secondNode = nodes.get(secondNodeName)
 
   // Make sure to update the connections state array, also add the whole nodeData instead of just what's needed
 
-  return (!(connectionData?.operation === "delete" || connectionData?.operation === "hide") ?
-    <>
-      <Line points={[firstNode.ui.x, firstNode.ui.y, firstNode.ui.x, secondNode.ui.y]} stroke="black" strokeWidth={1}/>
-      <Line points={[secondNode.ui.x, secondNode.ui.y, firstNode.ui.x, secondNode.ui.y]} stroke="black" strokeWidth={1}/>
-    </> : <></>
-  )
+  return ((secondNode && firstNode) ?<>
+    <Line points={[firstNode.ui.x, firstNode.ui.y, firstNode.ui.x, secondNode.ui.y]} stroke="black" strokeWidth={1}/>
+    <Line points={[secondNode.ui.x, secondNode.ui.y, firstNode.ui.x, secondNode.ui.y]} stroke="black" strokeWidth={1}/>
+  </> : null)
 }
 
 export default ConnectedLines
