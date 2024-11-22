4bae8d0c8d38a6a653200867ec4fde6ca9acd409
Tayomide <tibrahim@hawk.iit.edu>
2024-11-22 03:17:11 -0600

    Rewrite navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Avatar, Typography, Button, Divider } from '@mui/material';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { useContext } from 'react';
+import styled from 'styled-components';
+import { Link, useLocation } from 'react-router-dom';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { AuthContext } from "../hooks/AuthContext";
 import ProfileIcon from '../assets/icons/image 4.svg';
 import SystemsOverviewIcon from '../assets/icons/image 5.svg';
@@ -10,88 +9,131 @@ import LogIcon from '../assets/icons/image 6.svg';
 import NodeIcon from '../assets/icons/image 9.svg';
 const drawerWidth = 238;
 const Navbar = ({username}) => {
-    const { setUser } = useContext(AuthContext)
-    const location = useLocation();
-    const logout = () => {
-      const query = `
-        query {
-          logoutUser {
-            message
-            status
-          }
+  const location = useLocation();
+  const { setUser, user } = useContext(AuthContext)
+  const logout = () => {
+    const token = localStorage.getItem("token")
+    const query = `
+      query {
+        logoutUser {
+          message
+          status
         }
-      `
-  
-      fetch(`${import.meta.env.VITE_SERVER_URI}/graphql`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify({ query })
-      }).then(
-        res => res.json()
-      ).then(
-        res => {
-          setUser(null)
-          console.log(res)
-        }
-      ) 
-    }
-  return (
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        background: '#E6E6E6',
-        '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          boxSizing: 'border-box',
-          background: 'var(--d-9-d-9-d-9, #E6E6E6)',
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-    >
-      <Box sx={{ display: 'flex', alignItems: 'center', padding: 2 }}>
-      <img src={ProfileIcon} width='51px' height='51px'/>
-        <Box sx={{ ml: 2 }}>
-        <Typography variant="h6">{username}</Typography>
-          <Button onClick={logout} endIcon={<ExitToAppIcon />}>
-            Log out
-          </Button>
-        </Box>
-      </Box>
-      <Divider />
-      <List>
-        <ListItem button component={Link} to="/prototype1">
-          <ListItemIcon>
-            <img src={SystemsOverviewIcon} width='48px' height='48px'/>
-          </ListItemIcon>
-          <ListItemText  primary={
-              <Typography variant="body1" sx={{ fontWeight: location.pathname === "/prototype1" ? 'bold' : 'normal' }}>
-                System Overview
-              </Typography>
-            }/>
-        </ListItem>
-        <ListItem button component={Link} to="/logs">
-          <ListItemIcon><img src={LogIcon} width='48px' height='48px'/></ListItemIcon>
-          <ListItemText primary={
-            <Typography variant="body1" sx={{ fontWeight: location.pathname === "/logs" ? 'bold' : 'normal' }}>
-              Log System
-            </Typography>
-          } />
-        </ListItem>
-        <ListItem button component={Link} to="/buildings">
-          <ListItemIcon><img src={NodeIcon} width='48px' height='48px'/></ListItemIcon>
-          <ListItemText primary={
-            <Typography variant="body1" sx={{ fontWeight: location.pathname === "/buildings" ? 'bold' : 'normal' }}>
-              Node Configuration
-            </Typography>
-          } />
-        </ListItem>
-      </List>
-    </Drawer>
+      }
+    `
+
+    fetch(`${import.meta.env.VITE_SERVER_URI}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      credentials: 'include',
+      body: JSON.stringify({ query })
+    }).then(
+      res => res.json()
+    ).then(
+      res => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("expiresIn")
+        setUser(false)
+      }
+    ) 
+  }
+  return ( location.pathname !== "/login" ?
+    <Container>
+      <li className='profile'>
+        <img src={ProfileIcon} width='35px' height='35px'/>
+        <p>{user?.name || user?.email || ""}</p>
+      </li>
+      <li className="link">
+        <Link to="/buildings">
+          <img src={SystemsOverviewIcon} width='30px' height='30px'/>
+          <p>Systems overview</p>
+        </Link>
+      </li>
+      <li className="link">
+        <Link to="/logs">
+          <img src={LogIcon} width='30px' height='30px'/>
+          <p>Logs</p>
+        </Link>
+      </li>
+      {/* <li>
+        <img src={NodeIcon} width='51px' height='51px'/>
+        <p>Node Configuration</p>
+      </li> */}
+      <li className='logout'>
+        <button onClick={logout}>
+          Log out
+          <ExitToAppIcon />
+        </button>
+      </li>
+    </Container> : <></>
   );
 };
+
+const Container = styled.ul`
+  background-color: #ebeef3;
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  margin: 0;
+  position: sticky;
+  top: 0;
+  height: 100vh;
+  li{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 0.5em;
+    &.link{
+      cursor: pointer;
+      &:hover{
+        background-color: #d5d5d5;
+      }
+    }
+    a{
+      display: flex;
+      padding: 1em;
+      flex: 1;
+      flex-direction: row;
+      align-items: center;
+      gap: 0.5em;
+      font-size: 1.12em;
+      text-decoration: none;
+      color: inherit;
+    }
+    button{
+      font-family: 'Times New Roman', Times, serif;
+      flex: 1;
+      outline: 0;
+      border: 0;
+      background: transparent;
+      display: flex;
+      cursor: pointer;
+      flex-direction: row;
+      align-items: center;
+      gap: 1em;
+      text-decoration: none;
+      color: inherit;
+      font-size: 1.2em;
+      height: max-content;
+      justify-content: space-between;
+      padding: 0.7em;
+    }
+  }
+  li.profile{
+    padding: 1em;
+  }
+  li.logout{
+    flex: 1;
+    height: max-content;
+    align-items: flex-end;
+    font-size: 1em;
+    button:hover{
+      background-color: #d5d5d5;
+    }
+  }
+`
 export default Navbar;
