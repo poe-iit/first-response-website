262bafdfa5464ce2ab311d5ab175e3585f7ac13a
Tayomide <tibrahim@hawk.iit.edu>
2024-11-21 18:03:33 -0600

    Make page update realtime

--- src/pages/Buildings.jsx
+++ src/pages/Buildings.jsx
@@ -1,10 +1,68 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
-import Building from '../components/Building'
+import Floor from '../components/Floor'
 import { Link } from 'react-router-dom'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CreateBuilding from '../components/CreateBuilding';
+
+const generateRandomId = () => {
+  return Math.random().toString(36).substring(2, 9);
+}
 
 const Buildings = () => {
   const [buildings, setBuildings] = useState([])
+  const [creatingBuilding, setCreatingBuilding] = useState(false)
+  const wsRef = useRef(null)
+
+  const connectWebSocket = () => {
+    wsRef.current = new WebSocket(`${import.meta.env.VITE_SERVER_URI}`, "graphql-transport-ws")
+    const subscription = `
+      subscription{
+        buildingUpdates{
+          id
+          name
+          floors{
+            id
+            name
+          }
+        }
+      }
+    `
+
+    wsRef.current.onopen = () => {
+      console.log("Connected to WebSocket")
+      wsRef.current.send(JSON.stringify({
+        "type": "connection_init"
+      }))
+      const id = generateRandomId()
+      wsRef.current.send(JSON.stringify({
+        "id": id,
+        "type": "subscribe",
+        "payload": {
+          "query": subscription
+        }
+      }))
+    }
+    wsRef.current.onmessage = (event) => {
+      console.log(event)
+      const data = JSON.parse(event.data)?.payload?.data || {}
+      if(data?.buildingUpdates){
+        setBuildings(data.buildingUpdates)
+      }
+    }
+    wsRef.current.onclose = () => {
+      reconnectIfNeeded()
+    }
+  }
+  const reconnectIfNeeded = () => {
+    console.log("Reconnecting...")
+    if (wsRef.current && wsRef.current.readyState === WebSocket.CLOSED) {
+      connectWebSocket()
+    }
+  }
   const getBuildings = () => {
     const token = localStorage.getItem("token")
     const query = `
@@ -40,56 +98,85 @@ const Buildings = () => {
         console.log(err)
       }
     )
+
+    connectWebSocket()
   }
 
   useEffect(() => {
     getBuildings()
+    document.addEventListener("visibilitychange", reconnectIfNeeded)
+    return () => {
+      document.removeEventListener("visibilitychange", reconnectIfNeeded)
+    }
   }, [])
   return (
     // Add option to delete floors and buildings
     <Container>
-      <h3>This definitely needs to be redesigned</h3>
-      <p>I'm just dumping things here because I need the links</p>
-      <ul>
-        {
-          buildings.map((building, key) => <Building key={key} id={building?.id} name={building?.name} floors={building?.floors} setBuildings={setBuildings} />)
-        }
-      </ul>
-      <Link to="/building/new" className='new-building'>Create a new Building</Link>
+      <h2>System Overview</h2>
+      {
+        buildings.map((building, key) => (
+          <Accordion className="building" key={key}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Link to={`/building/${building?.id}`} className='building-name'>{building?.name}</Link>
+            </AccordionSummary>
+            <AccordionDetails>
+              <ul>
+              {
+                building?.floors?.length ? building.floors.map((floor, key) => <Floor key={key} name={floor.name} id={floor.id} setBuildings={setBuildings} /> ): <></>
+              }
+              </ul>
+              <Link to={`/building/${building?.id}/new`} className='new-floor'>Create a new floor</Link>
+            </AccordionDetails>
+          </Accordion>
+        ))
+      }
+      <button className='new-building' onClick={() => setCreatingBuilding(true)}>Create a new building</button>
+      {creatingBuilding ? <CreateBuilding setCreatingBuilding={setCreatingBuilding} /> : <></>}
     </Container>
   )
 }
 
 const Container = styled.div`
   min-height: 100vh;
-  > ul{
-    list-style: none;
-    padding: 0 0 1em 1em;
-    li {
-      width: max-content;
-      a:not(.new-floor){
-        padding: 0.5em;
-        text-decoration: none;
-        &:hover{
-          text-decoration: underline;
-        }
-      }
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h2{
+    margin: 1em 0;
   }
-
   a{
     display: block;
-    color: #0000ee;
+    color: #3a3aff;
+    text-decoration: none;
   }
-
+  .building-name{
+    &:hover{
+      text-decoration: underline;
+    }
+  }
   .new-building, .new-floor{
-    margin-left: 1em;
     text-decoration: none;
     color: white;
     background-color: #3a3aff;
     padding: 0.5em;
     border-radius: 0.5em;
-    width: max-content;
+    width: 100%;
+    text-align: center;
+  }
+  .new-building{
+    font-size: 1em;
+    width: 80%;
+    margin-top: 1em;
+    outline: 0;
+    border: 0;
+    cursor: pointer;
+  }
+  .building{
+    width: 80%;
   }
 `
 
